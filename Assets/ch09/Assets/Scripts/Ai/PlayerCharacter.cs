ffda52ea0e9d65a57d73cc0e7cc0d56579ad17ac
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-06 15:07:44 -0700

    feat: Finish 12.1.1

---
+++ Assets/ch09/Assets/Scripts/Ai/PlayerCharacter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public void Hurt(int damage)
+    {
+        Managers.Player.ChangeHealth(-damage);
+    }
+}
