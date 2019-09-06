6fbf65aa8ddda6b7e78a10f9806dee8fe4f5e38a
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-06 15:28:14 -0700

    feat: Complete 12.1.2

--- Assets/ch09/Assets/Scripts/Devices/ColorChangeDevice.cs
+++ Assets/ch09/Assets/Scripts/Devices/ColorChangeDevice.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class ColorChangeDevice : MonoBehaviour {
-	public void Operate() {
-		Color random = new Color(Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f));
-		GetComponent<Renderer>().material.color = random;
-	}
+public class ColorChangeDevice : BaseDevice
+{
+    public override void Operate()
+    {
+        Color random = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        GetComponent<Renderer>().material.color = random;
+    }
 }
