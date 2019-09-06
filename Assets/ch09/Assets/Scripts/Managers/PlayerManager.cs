c7880077ec9e88fc9a025f31678cd796b142505a
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-09-06 14:59:15 -0700

    feat: Add network service code

---
+++ Assets/ch09/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status { get; private set; }
+
+    public int health { get; private set; }
+    public int maxHealth { get; private set; }
+
+    private NetworkService _network;
+
+    public void Startup(NetworkService service)
+    {
+        Debug.Log("Player manager starting...");
+
+        _network = service;
+
+        // these values could be initialized with saved data
+        health = 50;
+        maxHealth = 100;
+
+        // any long-running startup tasks go here, and set status to 'Initializing' until those tasks are complete
+        status = ManagerStatus.Started;
+    }
+
+    public void ChangeHealth(int value)
+    {
+        health += value;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else if (health < 0)
+        {
+            health = 0;
+        }
+
+        Debug.Log("Health: " + health + "/" + maxHealth);
+    }
+}
